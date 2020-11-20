2857b85d17ce4d7c29fa563275bdb3c5a7cb07b3
Giulia Carli <giulia.carli@avanade.com>
2020-11-20 16:43:24 +0100

    ado.net

--- ADO/Program.cs
+++ ADO/Program.cs
@@ -10,7 +10,7 @@ namespace ADO
             //ConnectedMode.ConnectedStoredProcedure();
             //ConnectedMode.ConnectedScalar();
             DisconnectedMode.Disconnected();
-            ConnectedMode.Connected();
+            ConnectedMode.Connected(); 
         }
     }
 }
